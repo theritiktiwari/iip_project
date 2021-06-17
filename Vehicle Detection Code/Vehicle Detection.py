ce4739da3c0142165c1191a21771ac9b74a53692
Ritik Tiwari <59665768+theritiktiwari@users.noreply.github.com>
2021-06-17 22:16:31 +0530

    Update Vehicle Detection.py

--- Vehicle Detection Code/Vehicle Detection.py
+++ Vehicle Detection Code/Vehicle Detection.py
@@ -12,14 +12,13 @@ while True:
     cars = car_cascade.detectMultiScale(gray, 1.1, 9)
     for (x,y,w,h) in cars:
             plate = frame[y:y + h, x:x + w]
-            cv2.rectangle(frame,(x,y),(x +w, y +h) ,(51 ,51,255),2)
-            cv2.rectangle(frame, (x, y - 40), (x + w, y), (51,51,255), -2)
-            cv2.putText(frame, 'Car', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+            cv2.rectangle(frame,(x,y),(x + w, y + h) ,(255 , 0, 0),2)
+            cv2.rectangle(frame, (x, y - 30), (x + w, y), (255, 0, 0), -2)
+            cv2.putText(frame, 'Vehicle', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
             cv2.imshow('Vehicle',plate)
-    frames = cv2.resize(frame,(1152,700))
+    frames = cv2.resize(frame,(1920,1080))
     cv2.imshow('Vehicle Detection System', frames)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
+    if cv2.waitKey(1) & 0xff == ord('q'):
         break
 
 cap.release()
